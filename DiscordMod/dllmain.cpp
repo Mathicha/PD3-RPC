b322ea6b0e8b666b6399ccf833141f623e43d3ca
Mathi <11136549+Mathicha@users.noreply.github.com>
2023-10-12 17:07:43 +0200

    fix in heist party size and some nullptr crash issues

--- DiscordMod/dllmain.cpp
+++ DiscordMod/dllmain.cpp
@@ -32,7 +32,7 @@ public:
     std::optional<StringType> heist_name = std::nullopt;
     std::optional<StringType> difficulty_name = std::nullopt;
     // std::optional<UObject *> SBZGameInstance = std::nullopt;
-    std::optional<int> party_size = std::nullopt;
+    std::optional<int> party_size = 1;
     time_t startTimestamp = std::time(0);
 
     DiscordMod() : CppUserModBase()
@@ -74,7 +74,6 @@ public:
             break;
         case Status::InPrePlanning:
             discordPresence.state = "In Pre-Planning";
-            // TODO: get heist & difficulty for pre-planning
             break;
         case Status::InHeist:
             discordPresence.state = "In Heist";
@@ -109,7 +108,7 @@ public:
             [=](AActor *Context)
             {
                 auto classPrivate = Context->GetClassPrivate();
-                if (!classPrivate)
+                if (classPrivate == nullptr)
                     return;
                 auto name = classPrivate->GetName();
                 // Output::send<LogLevel::Verbose>(STR("[Discord] BeginPlayPostCallback: {}\n"), name);
@@ -167,33 +166,50 @@ public:
 
         // Output::send<LogLevel::Verbose>(STR("[Discord] on_update\n"));
 
-        // TODO: move this somewhere else? maybe hook WBP_UI_HUD_Overlay_Right_C:OnInitialized?
-        if (status == Status::InHeist)
+        if (status == Status::InMenu)
         {
+            auto SBZPartyManager = UObjectGlobals::FindFirstOf(STR("SBZPartyManager"));
+
+            if (SBZPartyManager != nullptr)
+            {
+                auto PartyMembers = SBZPartyManager->GetValuePtrByPropertyNameInChain<Unreal::TArray<SBZPartyMember>>(STR("PartyMembers"));
+                if (PartyMembers != nullptr)
+                    party_size = PartyMembers->Num();
+                else
+                    party_size = 1;
+            }
+        }
+        else if (status == Status::InHeist)
+        {
+            // TODO: move this somewhere else? maybe hook WBP_UI_HUD_Overlay_Right_C:OnInitialized?
             auto WBP_UI_HUD_Overlay_Right_C = UObjectGlobals::FindObject(
                 STR("WBP_UI_HUD_Overlay_Right_C"),
                 nullptr,
                 EObjectFlags::RF_NoFlags,
                 EObjectFlags::RF_ClassDefaultObject | EObjectFlags::RF_WasLoaded | EObjectFlags::RF_LoadCompleted);
 
-            if (WBP_UI_HUD_Overlay_Right_C)
+            if (WBP_UI_HUD_Overlay_Right_C != nullptr)
             {
                 auto Text_Difficulty = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UClass *>(STR("Text_Difficulty"));
-                if (Text_Difficulty)
+                if (Text_Difficulty != nullptr)
                 {
                     auto Text = Text_Difficulty->GetValuePtrByPropertyName<FText>(STR("Text"));
-                    if (Text)
+                    if (Text != nullptr)
                         difficulty_name = Text->ToString();
+                    else
+                        difficulty_name = std::nullopt;
                 }
                 else
                     difficulty_name = std::nullopt;
 
                 auto Text_LevelName = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UClass *>(STR("Text_LevelName"));
-                if (Text_LevelName)
+                if (Text_LevelName != nullptr)
                 {
                     auto Text = Text_LevelName->GetValuePtrByPropertyName<FText>(STR("Text"));
-                    if (Text)
+                    if (Text != nullptr)
                         heist_name = Text->ToString();
+                    else
+                        heist_name = std::nullopt;
                 }
                 else
                     heist_name = std::nullopt;
@@ -203,23 +219,26 @@ public:
                 difficulty_name = std::nullopt;
                 heist_name = std::nullopt;
             }
-        }
 
-        if (status != Status::GameStarting)
-        {
-            auto SBZPartyManager = UObjectGlobals::FindFirstOf(STR("SBZPartyManager"));
+            auto PD3HUDPartyContainerWidget = UObjectGlobals::FindFirstOf(STR("PD3HUDPartyContainerWidget"));
 
-            if (SBZPartyManager)
+            if (PD3HUDPartyContainerWidget != nullptr)
             {
-                auto PartyMembers = *SBZPartyManager->GetValuePtrByPropertyNameInChain<Unreal::TArray<SBZPartyMember>>(STR("PartyMembers"));
-                if (PartyMembers.Num())
-                    party_size = PartyMembers.Num();
-                else
-                    party_size = std::nullopt;
+                auto Panel_PartyPlayerWidgetContainer = *PD3HUDPartyContainerWidget->GetValuePtrByPropertyNameInChain<UClass *>(STR("Panel_PartyPlayerWidgetContainer"));
+                if (Panel_PartyPlayerWidgetContainer != nullptr)
+                {
+                    auto Slots = Panel_PartyPlayerWidgetContainer->GetValuePtrByPropertyNameInChain<Unreal::TArray<UObject *>>(STR("Slots"));
+                    if (Slots != nullptr)
+                        party_size = Slots->Num() + 1;
+                    else
+                        party_size = 1;
+                }
             }
-            else
-                party_size = std::nullopt;
         }
+        /* else if (status == Status::InPrePlanning)
+        {
+            // TODO: get heist, difficulty and players from pre-planning screen
+        } */
 
         update_presence();
     }
