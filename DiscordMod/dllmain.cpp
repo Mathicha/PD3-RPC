a210d3c971a3ffff2c6d645b42b9a4cc068ac019
Mathi <11136549+Mathicha@users.noreply.github.com>
2024-06-28 17:38:22 +0200

    wip join button from discord

--- DiscordMod/dllmain.cpp
+++ DiscordMod/dllmain.cpp
@@ -6,17 +6,28 @@
 
 #include "discord_rpc.h"
 
-#define APPLICATION_ID "1158533300550369470"
+#define DISCORD_APPLICATION_ID "1158533300550369470"
+#define PAYDAY_3_STEAM_APPID "1272080"
 
 using namespace RC;
 using namespace RC::Unreal;
 
-struct SBZPartyMember
+struct FSBZPartyMember
 {
     /* ... */
 };
 
-enum Status
+enum ESBZOnlineJoinType : uint8_t
+{
+    Debug = 0,
+    Public = 1,
+    Private = 2,
+    FriendsOnly = 3,
+    InviteOnly = 4,
+    Default = 1,
+};
+
+enum Status : uint8_t
 {
     GameStarting,
     InMenu,
@@ -25,13 +36,17 @@ enum Status
     InHeist
 };
 
+std::wstring constchartowstring(const char *src)
+{
+    return std::wstring(src, src + strlen(src));
+}
+
 class DiscordMod : public RC::CppUserModBase
 {
 public:
     Status status = Status::GameStarting;
     std::optional<StringType> heist_name = std::nullopt;
     std::optional<StringType> difficulty_name = std::nullopt;
-    // std::optional<UObject *> SBZGameInstance = std::nullopt;
     std::optional<int> party_size = 1;
     time_t startTimestamp = std::time(0);
 
@@ -50,12 +65,19 @@ public:
         { Output::send<LogLevel::Verbose>(STR("[Discord:Event] Disconnected (errcode={})\n"), errorCode); };
         handlers.errored = [](int errorCode, const char *message)
         { Output::send<LogLevel::Verbose>(STR("[Discord:Event] Error (errcode={})\n"), errorCode); };
-        Discord_Initialize(APPLICATION_ID, &handlers, 1, NULL);
+        handlers.joinGame = [](const char *joinSecret)
+        { Output::send<LogLevel::Verbose>(STR("[Discord:Event] Join game {}\n"), constchartowstring(joinSecret)); };
+        handlers.joinRequest = [](const DiscordUser *request)
+        {
+            Output::send<LogLevel::Verbose>(STR("[Discord:Event] Join request from {}#{} - {}\n"), constchartowstring(request->username), constchartowstring(request->discriminator), constchartowstring(request->userId));
+            Discord_Respond(request->userId, DISCORD_REPLY_IGNORE);
+        };
+        Discord_Initialize(DISCORD_APPLICATION_ID, &handlers, 1, PAYDAY_3_STEAM_APPID);
 
         Output::send<LogLevel::Verbose>(STR("[Discord] Init.\n"));
     }
 
-    ~DiscordMod()
+    ~DiscordMod() override
     {
         Discord_Shutdown();
     }
@@ -91,9 +113,17 @@ public:
         discordPresence.startTimestamp = startTimestamp;
         discordPresence.largeImageKey = "pd3"; // TODO: add heist images
         // discordPresence.smallImageKey = "pd3"; // TODO: character mask images
+
         if (party_size.has_value())
             discordPresence.partySize = party_size.value();
+        else
+            discordPresence.partySize = 1;
         discordPresence.partyMax = 4; // TODO: non hard coded party max
+
+        discordPresence.partyPrivacy = DISCORD_PARTY_PUBLIC; // TODO: private if offline or (ESBZOnlineJoinType) Private FriendsOnly InviteOnly
+        discordPresence.joinSecret = "test";                 // TODO: get the partycode(?) or idk whats needed exactly to join payday lobby (max 128 bytes)
+        discordPresence.partyId = "party";                   // TODO: get something unique that identifies the party (but not the code) (max 128 bytes)
+
         Discord_UpdatePresence(&discordPresence);
 
 #ifdef DISCORD_DISABLE_IO_THREAD
@@ -129,27 +159,6 @@ public:
                     startTimestamp = std::time(0);
                 }
             });
-
-        /* auto SBZGameInstanceClass = UObjectGlobals::StaticFindObject<UClass *>(nullptr, nullptr, STR("/Script/Starbreeze.SBZGameInstance"));
-
-        Hook::RegisterStaticConstructObjectPostCallback(
-            [=](const FStaticConstructObjectParameters &, UObject *constructed_object)
-            {
-                UStruct *object_class = constructed_object->GetClassPrivate();
-                while (object_class)
-                {
-
-                    if (object_class == SBZGameInstanceClass)
-                    {
-                        SBZGameInstance = static_cast<UObject *>(constructed_object);
-                        // Output::send<LogLevel::Verbose>(STR("[Discord] SBZGameInstance: {}\n"), PlayerState->GetFullName());
-                        break;
-                    }
-
-                    object_class = object_class->GetSuperStruct();
-                }
-                return constructed_object;
-            }); */
     }
 
     // lazy delay for on_update
@@ -164,20 +173,22 @@ public:
         else
             on_update_id = 0;
 
-        // Output::send<LogLevel::Verbose>(STR("[Discord] on_update\n"));
-
         if (status == Status::InMenu)
         {
             auto SBZPartyManager = UObjectGlobals::FindFirstOf(STR("SBZPartyManager"));
 
             if (SBZPartyManager != nullptr)
             {
-                auto PartyMembers = SBZPartyManager->GetValuePtrByPropertyNameInChain<Unreal::TArray<SBZPartyMember>>(STR("PartyMembers"));
+                auto PartyMembers = SBZPartyManager->GetValuePtrByPropertyNameInChain<Unreal::TArray<FSBZPartyMember>>(STR("PartyMembers"));
                 if (PartyMembers != nullptr)
                     party_size = PartyMembers->Num();
                 else
                     party_size = 1;
             }
+        }
+        else if (status == Status::InPrePlanning)
+        {
+            // TODO: get heist, difficulty and players in pre-planning screen
         }
         else if (status == Status::InHeist)
         {
@@ -190,7 +201,7 @@ public:
 
             if (WBP_UI_HUD_Overlay_Right_C != nullptr)
             {
-                auto Text_Difficulty = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UClass *>(STR("Text_Difficulty"));
+                auto Text_Difficulty = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UObject *>(STR("Text_Difficulty"));
                 if (Text_Difficulty != nullptr)
                 {
                     auto Text = Text_Difficulty->GetValuePtrByPropertyName<FText>(STR("Text"));
@@ -202,7 +213,7 @@ public:
                 else
                     difficulty_name = std::nullopt;
 
-                auto Text_LevelName = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UClass *>(STR("Text_LevelName"));
+                auto Text_LevelName = *WBP_UI_HUD_Overlay_Right_C->GetValuePtrByPropertyNameInChain<UObject *>(STR("Text_LevelName"));
                 if (Text_LevelName != nullptr)
                 {
                     auto Text = Text_LevelName->GetValuePtrByPropertyName<FText>(STR("Text"));
@@ -215,16 +226,13 @@ public:
                     heist_name = std::nullopt;
             }
             else
-            {
-                difficulty_name = std::nullopt;
-                heist_name = std::nullopt;
-            }
+                difficulty_name = heist_name = std::nullopt;
 
             auto PD3HUDPartyContainerWidget = UObjectGlobals::FindFirstOf(STR("PD3HUDPartyContainerWidget"));
 
             if (PD3HUDPartyContainerWidget != nullptr)
             {
-                auto Panel_PartyPlayerWidgetContainer = *PD3HUDPartyContainerWidget->GetValuePtrByPropertyNameInChain<UClass *>(STR("Panel_PartyPlayerWidgetContainer"));
+                auto Panel_PartyPlayerWidgetContainer = *PD3HUDPartyContainerWidget->GetValuePtrByPropertyNameInChain<UObject *>(STR("Panel_PartyPlayerWidgetContainer"));
                 if (Panel_PartyPlayerWidgetContainer != nullptr)
                 {
                     auto Slots = Panel_PartyPlayerWidgetContainer->GetValuePtrByPropertyNameInChain<Unreal::TArray<UObject *>>(STR("Slots"));
@@ -235,10 +243,6 @@ public:
                 }
             }
         }
-        /* else if (status == Status::InPrePlanning)
-        {
-            // TODO: get heist, difficulty and players from pre-planning screen
-        } */
 
         update_presence();
     }
